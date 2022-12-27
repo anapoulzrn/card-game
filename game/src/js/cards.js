52afaaf55a2e3995d37b7a61d9059bd9ea067540
anabang <anapoulzrn@gmail.com>
2022-12-28 00:01:36 +0600

    hw3

---
+++ game/src/js/cards.js
@@ -0,0 +1,186 @@
+const cardsArr = [
+    {
+        card: 'рубашка',
+        src: 'src/img/рубашка.svg',
+    },
+
+    {
+        card: '6 крести',
+        src: 'src/img/6_крести.svg',
+    },
+
+    {
+        card: '7 крести',
+        src: 'src/img/7 крести.svg',
+    },
+
+    {
+        card: '8 крести',
+        src: 'src/img/8 крести.svg',
+    },
+
+    {
+        card: '9 крести',
+        src: 'src/img/9 крести.svg',
+    },
+
+    {
+        card: '10 крести',
+        src: 'src/img/10 крести.svg',
+    },
+
+    {
+        card: 'валет крести',
+        src: 'src/img/валет крести.svg',
+    },
+
+    {
+        card: 'дама крести',
+        src: 'src/img/дама крести.svg',
+    },
+
+    {
+        card: 'король крести',
+        src: 'src/img/король крести.svg',
+    },
+
+    {
+        card: 'туз крести',
+        src: 'src/img/туз крести.svg',
+    },
+
+    {
+        card: '6 пики',
+        src: 'src/img/6 пики.svg',
+    },
+
+    {
+        card: '7 пики',
+        src: 'src/img/7 пики.svg',
+    },
+
+    {
+        card: '8 пики',
+        src: 'src/img/8 пики.svg',
+    },
+
+    {
+        card: '9 пики',
+        src: 'src/img/9 пики.svg',
+    },
+
+    {
+        card: '10 пики',
+        src: 'src/img/10 пики.svg',
+    },
+
+    {
+        card: 'валет пики',
+        src: 'src/img/валет пики.svg',
+    },
+
+    {
+        card: 'дама пики',
+        src: 'src/img/дама пики.svg',
+    },
+
+    {
+        card: 'король пики',
+        src: 'src/img/король пики.svg',
+    },
+
+    {
+        card: 'туз пики',
+        src: 'src/img/туз пики.svg',
+    },
+
+    {
+        card: '6 черви',
+        src: 'src/img/6 черви.svg',
+    },
+
+    {
+        card: '7 черви',
+        src: 'src/img/7 черви.svg',
+    },
+
+    {
+        card: '8 черви',
+        src: 'src/img/8 черви.svg',
+    },
+
+    {
+        card: '9 черви',
+        src: 'src/img/9 черви.svg',
+    },
+
+    {
+        card: '10 черви',
+        src: 'src/img/10 черви.svg',
+    },
+
+    {
+        card: 'валет черви',
+        src: 'src/img/валет черви.svg',
+    },
+
+    {
+        card: 'дама крести',
+        src: 'src/img/дама черви.svg',
+    },
+
+    {
+        card: 'король черви',
+        src: 'src/img/король черви.svg',
+    },
+
+    {
+        card: 'туз черви',
+        src: 'src/img/туз черви.svg',
+    },
+
+    {
+        card: '6 бубны',
+        src: 'src/img/6 бубны.svg',
+    },
+
+    {
+        card: '7 бубны',
+        src: 'src/img/7 бубны.svg',
+    },
+
+    {
+        card: '8 бубны',
+        src: 'src/img/8 бубны.svg',
+    },
+
+    {
+        card: '9 бубны',
+        src: 'src/img/9 бубны.svg',
+    },
+
+    {
+        card: '10 бубны',
+        src: 'src/img/10 бубны.svg',
+    },
+
+    {
+        card: 'валет бубны',
+        src: 'src/img/валет бубны.svg',
+    },
+
+    {
+        card: 'дама бубны',
+        src: 'src/img/дама бубны.svg',
+    },
+
+    {
+        card: 'король бубны',
+        src: 'src/img/король бубны.svg',
+    },
+
+    // {
+    //     card: 'туз бубны',
+    //     src: 'src/img/туз бубны.svg',
+    // },
+]
--- game/src/js/game.js
+++ game/src/js/game.js
@@ -1,11 +1,65 @@
 function renderGameBlock(container) {
-    console.log(container);
+    const cardsBox = document.createElement('div');
+    cardsBox.classList.add('cards-box');
+    for (let i = 0; i < cardsArr.length; i++) {
+        const card = document.createElement('img');
+        const url = cardsArr[i].src;
+        card.setAttribute('src', `${url}`);
+        card.classList.add('card-item');
+        cardsBox.appendChild(card);
+    }
+    container.appendChild(cardsBox)
 }
 
 window.application.blocks['gameBlock'] = renderGameBlock;
 
+function renderTimerBlock(container) {
+    const timerBox = document.createElement('div');
+    timerBox.classList.add('timer-box');
+
+    const timer = document.createElement('h1');
+    timer.textContent = '00.00';
+    timer.classList.add('timer');
+
+    const timeItems = document.createElement('div');
+    timeItems.classList.add('time-items');
+
+    const min = document.createElement('h3');
+    min.textContent = 'min';
+    min.classList.add('timer-item');
+
+    const sec = document.createElement('h3');
+    sec.textContent = 'sec';
+    sec.classList.add('timer-item');
+
+    timeItems.appendChild(min);
+    timeItems.appendChild(sec);
+
+    timerBox.appendChild(timeItems);
+    timerBox.appendChild(timer);
+
+    container.appendChild(timerBox);
+}
+
+window.application.blocks['timerBlock'] = renderTimerBlock;
+
 function renderGameScreen() {
-    console.log('тут будет экран игры');
+
+    app.style = 'padding: 0';
+
+    const button = document.createElement('button');
+    button.textContent = 'Начать заново';
+    button.classList.add('levels-btn');
+
+    const upperGameBlock = document.createElement('div');
+    upperGameBlock.classList.add('upper-game-block');
+    
+
+    window.application.renderBlock('timerBlock', upperGameBlock);
+    upperGameBlock.appendChild(button);
+    app.appendChild(upperGameBlock);
+    window.application.renderBlock('gameBlock', app);
+
 }
 
 window.application.screens['game'] = renderGameScreen;
